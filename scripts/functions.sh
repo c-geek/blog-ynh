55f73e3d678dece0d4d4585dde0f6cd973b795e1
Cédric Moreau <cem.moreau@gmail.com>
2017-02-07 10:32:48 +0100

    Create functions.sh

---
+++ scripts/functions.sh
@@ -0,0 +1,26 @@
+#/bin/bash
+
+INSTALL_CBLOG_PACKAGE () {
+
+  # Install python dependencies
+  sudo apt-get install -y python-pip python-virtualenv python-dev libldap2-dev libsasl2-dev libssl-dev
+  
+  # Create the virtualenv
+  virtualenv ~/virtualenvs/cblog && cd ~/virtualenvs/cblog && source bin/activate
+  
+  # Clone the cblog sources
+  git clone https://github.com/c-geek/blog && cd blog
+  SRC=`pwd`
+  
+  # Install pelican
+  pip install pelican pelican-youtube
+  
+  # Add the `medius` them
+  pelican-themes --install medius/
+  
+  # Generate the blog
+  pelican
+  
+  # Create a link to www/ folder
+  ln -s $SRC /var/www/cblog
+}
